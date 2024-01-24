ae7afcfabf0e727bf711fd5129dc119470fbfb28
KE-Holtz <keholtz08@gmail.com>
2024-01-23 19:10:15 -0500

    Added light manager class (to be upgraded) and light command functions

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,10 +10,13 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.DriveCommands;
+import frc.robot.commands.LightsCommands;
+import frc.robot.subsystems.LightsSubsystem;
 import frc.robot.subsystems.base.DriveBase;
 import frc.robot.subsystems.base.DriveBase.WheelModuleIndex;
 import frc.robot.subsystems.gyro.GyroIoPigeon2;
@@ -34,6 +37,7 @@ import frc.robot.subsystems.swerve.SwerveModuleIoTalonFx;
 public class RobotContainer {
 
   private final CommandXboxController controller = new CommandXboxController(0);
+  private final LightsSubsystem m_LightsSubsystem = new LightsSubsystem();
   private final DriveBase driveBase;
   private final LoggedDashboardChooser<Command> autoChooser;
 
@@ -147,6 +151,10 @@ public class RobotContainer {
                     new Pose2d(driveBase.getPose().getTranslation(), new Rotation2d())),
                 driveBase)
                 .ignoringDisable(true));
+    controller.a().whileTrue(new LightsCommands(m_LightsSubsystem, new Color(0,255,0)));
+    controller.b().whileTrue(new LightsCommands(m_LightsSubsystem, new Color(255,0,0)));
+    controller.x().whileTrue(new LightsCommands(m_LightsSubsystem, new Color(0,0, 255)));
+    controller.y().whileTrue(new LightsCommands(m_LightsSubsystem, new Color(255,255,0)));
   }
 
   /**
