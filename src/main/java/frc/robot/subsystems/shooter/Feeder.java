c842e5e3202f629b6190b1a1be9f30923cf069bf
X <>
2024-01-27 16:00:53 -0800

    Feeder

--- src/main/java/frc/robot/subsystems/shooter/Feeder.java
+++ src/main/java/frc/robot/subsystems/shooter/Feeder.java
@@ -9,6 +9,27 @@ import frc.robot.Constants;
 public class Feeder extends SubsystemBase {
     CANSparkMax feedMotorL = new CANSparkMax(Constants.LEFT_FEED_MOTOR_ID, MotorType.kBrushless);
     CANSparkMax feedMotorR = new CANSparkMax(Constants.RIGHT_FEED_MOTOR_ID, MotorType.kBrushless);
+
     public Feeder() {
+        feedMotorL.setInverted(false);
+        feedMotorR.setInverted(true);
     }
+
+    public void startFeeder() {
+        setVoltage(Constants.FEEDER_FORWARD_VOLTAGE);
+    }
+
+    public void stopFeeder() {
+        setVoltage(0.0);
+    }
+
+    public void reverseFeeder() {
+        setVoltage(Constants.FEEDER_REVERSE_VOLTAGE);
+    }
+
+    private void setVoltage(double voltage) {
+        feedMotorL.setVoltage(voltage);
+        feedMotorR.setVoltage(voltage);
+    }
+    // Add command methods here
 }
