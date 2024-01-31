05de5879c3bc00279e80767a5fe6b4e0aedf7f95
X <>
2024-01-30 18:16:25 -0800

    Added Wheel Drive Gear Ratio Variable to Constants Class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,11 +7,15 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -22,7 +26,7 @@ public final class Constants {
   public static final NeutralModeValue WHEEL_BRAKE_MODE = NeutralModeValue.Brake;
   public static final boolean FIELD_RELATIVE = false;
 
-  // ---------- Aliance ----------
+  // ---------- Alliance ----------
   // This is the side of the field that the aumation path are made for.
   public static final Alliance DEFAULT_ALLIANCE = Alliance.Blue;
   public static final boolean FLIP_PATH_IF_ALLIANCE_IS_NOT_DEFAULT = true;
@@ -36,19 +40,25 @@ public final class Constants {
   public static final double TRACK_WIDTH_X = Units.inchesToMeters(24.0);
   // Middle of left wheel to middle of right wheel.
   public static final double TRACK_WIDTH_Y = Units.inchesToMeters(24.0);
-  
-  // ---------- Wheel & GHear Measurements ----------
+
+  // ---------- Wheel & Gear Measurements ----------
   public static final double WHEEL_RADIUS = Units.inchesToMeters(2);
-  //public static final double WHEEL_DRIVE_GEAR_RATIO = (50.0 / 14.0) * (17.0 / 27.0) * (45.0 / 15.0);  //L2 Gear ratio
-  public static final double WHEEL_DRIVE_GEAR_RATIO = 6.12;  //L3 Gear ratio
+  // public static final double WHEEL_DRIVE_GEAR_RATIO = (50.0 / 14.0) * (17.0 /
+  // 27.0) * (45.0 / 15.0); //L2 Gear ratio
+  public static final double WHEEL_DRIVE_GEAR_RATIO = 6.12; // L3 Gear ratio
   public static final double WHEEL_TURN_GEAR_RATIO = 12.8;
 
-  // ---------- Wheel Rotation Offsets ---------- 
-  // Note: Chaning the Offset by Pie (180 degrees) will invert the direction the wheel spins.
+  // ---------- Wheel Rotation Offsets ----------
+  // Note: Chaning the Offset by Pie (180 degrees) will invert the direction the
+  // wheel spins.
   public static final Rotation2d FRONT_LEFT_ABSOLUTE_ENCODER_OFFSET = new Rotation2d(-2.26);
   public static final Rotation2d FRONT_RIGHT_ABSOLUTE_ENCODER_OFFSET = new Rotation2d(-0.35);
   public static final Rotation2d BACK_LEFT_ABSOLUTE_ENCODER_OFFSET = new Rotation2d(3.114);
   public static final Rotation2d BACK_RIGHT_ABSOLUTE_ENCODER_OFFSET = new Rotation2d(-3.0236);
+
+  // ---------- Game Piece Handling -------
+  public static final double FEEDER_FORWARD_VOLTAGE = 6.0;
+  public static final double FEEDER_REVERSE_VOLTAGE = -6.0;
 
   // ---------- Hardware Ids ----------
   public static final String CANIVORE_BUS_ID = "1559Canivore";
@@ -66,8 +76,8 @@ public final class Constants {
   public static final int BACK_RIGHT_DRIVE_MOTOR_ID = 6;
   public static final int BACK_RIGHT_STEER_MOTOR_ID = 7;
   public static final int BACK_RIGHT_CANCODER_ID = 8;
-  
-  //Roborio devices
+
+  // Roborio devices
   public static final int LEFT_FEED_MOTOR_ID = 20;
   public static final int RIGHT_FEED_MOTOR_ID = 21;
 
@@ -83,6 +93,7 @@ public final class Constants {
   }
 
   // ========================= Constructors ===================================
-  /** Makes this class non-instantiable.*/
-  private Constants() {}
+  /** Makes this class non-instantiable. */
+  private Constants() {
+  }
 }
