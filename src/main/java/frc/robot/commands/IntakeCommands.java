8f05e4e0056124069e21e0bb6b2538243fc379ac
Michael Marchetti <mike@themarchettis.com>
2024-02-02 22:19:36 -0500

    rename IntakeCommands

--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -1,12 +1,8 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.shooter.DualCanSparkMaxSubsystem;
-import frc.robot.subsystems.shooter.Flywheel;
-
-public class ShooterCommands {
+public class IntakeCommands {
   /** Makes Class non-instantiable */
-  private ShooterCommands() {
+  private IntakeCommands() {
   }
 
   // TODO
